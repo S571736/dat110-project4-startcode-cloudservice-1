c573e3fad81934f00e40dae20c90a2c11f9088ac
Sondre <Sondre.Gjesdal@gmail.com>
2021-05-05 15:35:58 +0200

    Added functionality for log

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -11,46 +11,54 @@ import com.google.gson.Gson;
 
 /**
  * Hello world!
- *
  */
 public class App {
-	
-	static AccessLog accesslog = null;
-	static AccessCode accesscode = null;
 
-	
-	public static void main(String[] args) {
+    static AccessLog accesslog = null;
+    static AccessCode accesscode = null;
 
-		if (args.length > 0) {
-			port(Integer.parseInt(args[0]));
-		} else {
-			port(8080);
-		}
 
-		// objects for data stored in the service
-		
-		accesslog = new AccessLog();
-		accesscode  = new AccessCode();
-		Gson gson = new Gson();
+    public static void main(String[] args) {
 
-		after((req, res) -> {
-  		  res.type("application/json");
-  		});
-		
-		// for basic testing purposes
-		get("/accessdevice/hello", (req, res) -> gson.toJson("IoT Access Control Device"));
-		
-		// TODO: implement the routes required for the access control service
-		// as per the HTTP/REST operations describined in the project description
+        if (args.length > 0) {
+            port(Integer.parseInt(args[0]));
+        } else {
+            port(8080);
+        }
 
-		get("/accessdevice/code",(req, res) -> gson.toJson(accesscode));
-		put("/accessdevice/code", (req,res) -> {
-			accesscode = gson.fromJson(req.body(), AccessCode.class);
-			accesscode.setAccesscode(accesscode.getAccesscode());
+        // objects for data stored in the service
 
-			return req.body();
-		});
-		
+        accesslog = new AccessLog();
+        accesscode = new AccessCode();
+        Gson gson = new Gson();
+
+        after((req, res) -> {
+            res.type("application/json");
+        });
+
+        // for basic testing purposes
+        get("/accessdevice/hello", (req, res) -> gson.toJson("IoT Access Control Device"));
+
+        post("/accessdevice/log", (req, res) -> accesslog.add(req.body()));
+        get("/accessdevice/log", (req, res) -> accesslog.toJson());
+        get("/accessdevice/log/:id", (req, res) -> gson.toJson(accesslog.get(Integer.parseInt(req.params(":id")))));
+
+        delete("/accessdevice/log", (req, res) -> {
+            accesslog.clear();
+            return gson.toJson("Log has been cleared");
+        });
+
+        // TODO: implement the routes required for the access control service
+        // as per the HTTP/REST operations describined in the project description
+
+        get("/accessdevice/code", (req, res) -> gson.toJson(accesscode));
+        put("/accessdevice/code", (req, res) -> {
+            accesscode = gson.fromJson(req.body(), AccessCode.class);
+            accesscode.setAccesscode(accesscode.getAccesscode());
+
+            return req.body();
+        });
+
     }
-    
+
 }
